6201ddc68ccbdb1aafe8b683ab95f22053cece6f
EricDuMingTong <155936646+EricDuMingTong@users.noreply.github.com>
2025-01-24 10:24:57 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation(fileTree(baseDir = "libs"))
+}
+
+group = "com.nothing.translatemerge"
+version = "1.2"
+
+repositories {
+    mavenCentral()
+}
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2024.1.7")
+    type.set("IC") // Target IDE Platform
+    pluginName.set("TranslateResMerger")
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("222")
+        untilBuild.set("291.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
